a0ee99e83a66aa59e37129739e9fc49960813b67
Seth Hubbell <hubbell@adobe.com>
2019-04-26 16:48:31 -0500

    Abstracting JiraBoardConfiguration

---
+++ source/web_api/utils/jira_helper/JiraBoardConfiguration.py
@@ -0,0 +1,5 @@
+class JiraBoardConfiguration:
+    def __init__(self, lead_time_start_state, lead_time_end_state, work_states):
+        self.lead_time_start_state = lead_time_start_state
+        self.lead_time_end_state = lead_time_end_state
+        self.work_states = work_states
