1aa68c802b6782f8b10a9f315795abf2ea40a678
Seth Hubbell <hubbell@adobe.com>
2019-04-26 15:58:24 -0500

    initial refactor for team_project.py

--- source/web_api/team_project/team_project.py
+++ source/web_api/team_project/team_project.py
@@ -1,9 +1,9 @@
 from __future__ import print_function
 import json
 import urllib
-from source.web_api.utils.redshift_connection.redshift_connection import RedshiftConnection
-from source.web_api.utils.api_response_helper import ApiResponseError
-from source.web_api.utils.api_response_helper import api_response_handler
+from utils.redshift_connection.redshift_connection import RedshiftConnection
+from utils.api_response_helper import ApiResponseError
+from utils.api_response_helper import api_response_handler
 
 
 def handler(event, context):
