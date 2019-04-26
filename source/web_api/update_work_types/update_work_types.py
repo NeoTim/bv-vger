596dbdf5c29b5002fb630ff1fa4e96c02f26234e
Seth Hubbell <hubbell@adobe.com>
2019-04-26 16:20:50 -0500

    initial refactor for update_work_types.py

--- source/web_api/update_work_types/update_work_types.py
+++ source/web_api/update_work_types/update_work_types.py
@@ -1,46 +1,49 @@
 import json
-from redshift_connection import RedshiftConnection
+from utils.redshift_connection.redshift_connection import RedshiftConnection
+from utils.api_response_helper import ApiResponseError
+from utils.api_response_helper import response_formatter
+from utils.api_response_helper import api_response_handler
 
-def response_formatter(status_code='400', body={'message': 'error'}):
-    api_response = {
-        'statusCode': status_code,
-        'headers': {
-            'Access-Control-Allow-Origin' : '*',
-            'Access-Control-Allow-Credentials' : True
-        },
-        'body': json.dumps(body)
-    }
-    return api_response
 
 def handler(event, context):
-    # Validate user input
-    try: 
-        # User input
-        data = json.loads(event['body'])
-        work_types = data
-    except:
-        payload = {'message': 'Invalid user input'}
-        return response_formatter(status_code='400', body=payload)
-    
-    # Parse project_id from the URL
+    return api_response_handler(__update_work_types, event)
+
+
+def __update_work_types(event):
+    work_types = __parse_lambda_input(event)
+    project_id = __parse_project_id_from_input(event)
+    __apply_work_type_updates(work_types=work_types, project_id=project_id)
+    return response_formatter(status_code=200, body={})
+
+
+def __parse_lambda_input(lambda_input):
     try:
-        project_id = (event.get('pathParameters').get('id'))
-    except:
+        # User input
+        return json.loads(lambda_input['body'])
+    except Exception:
+        payload = {'message': 'Invalid user input'}
+        raise ApiResponseError(status_code=400, body=payload)
+
+
+def __parse_project_id_from_input(lambda_input):
+    try:
+        return lambda_input.get('pathParameters').get('id')
+    except Exception:
         payload = {"message": "Could not get id path parameter"}
-        return response_formatter(status_code='400', body=payload)
-    
-    # Update work types
+        raise ApiResponseError(status_code=400, body=payload)
+
+
+def __apply_work_type_updates(work_types, project_id):
+    redshift = RedshiftConnection()
     try:
         insert_value_list = []
         for key in work_types:
             for issue in work_types[key]:
                 insert_value_list.append((project_id, issue, key))
-        redshift = RedshiftConnection()
         redshift.updateTeamWorkTypes(project_id, insert_value_list)
-    except:
+    except Exception:
+        redshift.closeConnection()
         payload = {'message': 'Internal error'}
-        return response_formatter(status_code='500', body=payload)
+        raise ApiResponseError(status_code=500, body=payload)
     finally:
         redshift.closeConnection()
-
-    return response_formatter(status_code='200', body={})
