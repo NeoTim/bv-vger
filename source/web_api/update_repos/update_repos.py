e30bb34f75c65a24d4e49b011b487d18bbe3f838
Seth Hubbell <hubbell@adobe.com>
2019-04-26 17:02:08 -0500

    initial refactor for update_repos.py

--- source/web_api/update_repos/update_repos.py
+++ source/web_api/update_repos/update_repos.py
@@ -1,63 +1,64 @@
 import json
-import boto3
-import os
 import requests
-from redshift_connection import RedshiftConnection
+from utils.redshift_connection.redshift_connection import RedshiftConnection
+from utils.api_response_helper import ApiResponseError
+from utils.api_response_helper import response_formatter
+from utils.api_response_helper import api_response_handler
 from source.git_etl.constants import git_etl_constants
 
 
-def response_formatter(status_code='400', body={'message': 'error'}):
-    api_response = {
-        'statusCode': status_code,
-        'headers': {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': True
-        },
-        'body': json.dumps(body)
-    }
-    return api_response
-
-
 def handler(event, context):
-    # Validate user input
+    return api_response_handler(__update_repos, event)
+
+
+def __update_repos(lambda_input):
+    repos = __parse_input(lambda_input=lambda_input)
+    project_id = __parse_project_id_from_input(lambda_input=lambda_input)
+    __validate_github_repos(repos)
+    __apply_github_repo_updates(repos=repos, project_id=project_id)
+    return response_formatter(status_code='200', body={})
+
+
+def __parse_input(lambda_input):
     try:
-        # User input
-        data = json.loads(event['body'])
-        repos = data
-    except:
+        return json.loads(lambda_input['body'])
+    except Exception:
         payload = {'message': 'Invalid user input'}
-        return response_formatter(status_code='400', body=payload)
+        raise ApiResponseError(status_code=400, body=payload)
 
-    # Parse project_id from the URL
+
+def __parse_project_id_from_input(lambda_input):
     try:
-        project_id = (event.get('pathParameters').get('id'))
-    except:
+        return lambda_input.get('pathParameters').get('id')
+    except Exception:
         payload = {"message": "Could not get id path parameter"}
-        return response_formatter(status_code='400', body=payload)
+        raise ApiResponseError(status_code=400, body=payload)
 
-    # Validate repository names
+
+def __validate_github_repos(repos):
     try:
         for repo in repos:
             GITHUB_API = git_etl_constants.GIT_REPO_URL.format(repo=repo)
-            r = requests.get(GITHUB_API, headers={'Authorization': 'token %s' % git_etl_constants.GIT_API_KEY})
-            if r.status_code != 200:
+            github_response = requests.get(GITHUB_API, headers={'Authorization': 'token %s' % git_etl_constants.GIT_API_KEY})
+            if github_response.status_code != 200:
                 payload = {'message': 'Invalid repository name: \'{}\''.format(repo)}
-                return response_formatter(status_code='400', body=payload)
-    except:
+                raise ApiResponseError(status_code=400, body=payload)
+    except Exception:
         # Git API fail
         payload = {'message': 'Service Unavailable'}
-        return response_formatter(status_code='503', body=payload)
+        raise ApiResponseError(status_code=503, body=payload)
 
+
+def __apply_github_repo_updates(repos, project_id):
+    database = RedshiftConnection()
     try:
-        redshift = RedshiftConnection()
         insert_repo_list = []
         for repo in repos:
             insert_repo_list.append((project_id, repo))
-        redshift.updateTeamRepos(project_id, insert_repo_list)
-    except:
+        database.updateTeamRepos(project_id, insert_repo_list)
+    except Exception:
+        database.closeConnection()
         payload = {'message': 'Internal error'}
-        return response_formatter(status_code='500', body=payload)
+        raise ApiResponseError(status_code=500, body=payload)
     finally:
-        redshift.closeConnection()
-
-    return response_formatter(status_code='200', body={})
+        database.closeConnection()
